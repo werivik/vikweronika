4a431fe433f68e9d2a1c6ddc90a33ff3347275fa
werivik <wervik00994@stud.noroff.no>
2025-01-01 19:42:59 +0100

    transitions

--- js/index/chatbox.js
+++ js/index/chatbox.js
@@ -2,21 +2,30 @@ document.addEventListener("DOMContentLoaded", () => {
     const chatbox1 = document.getElementById("chatbox1");
     const chatbox2 = document.getElementById("chatbox2");
     const loader = document.getElementById("loader");
+    const chatBobbles = document.getElementById("chatBobbles");
+    const profile = document.getElementById("myProfilePic");
 
     chatbox1.style.display = "block";
     chatbox2.style.display = "none";
     loader.style.display = "none";
+    chatBobbles.style.top = "35%";
+    profile.style.top = "34%";
 
     setTimeout(() => {
         chatbox1.classList.add("move-up");
-        loader.style.display = "block";
     }, 2700);
+
+    setTimeout(() => {
+        loader.style.display = "block";
+    }, 3000);
 
     setTimeout(() => {
         chatbox2.style.display = "block";
         chatbox1.classList.remove("move-up");
         loader.style.display = "none";
         chatbox1.style.transform = "translateY(-0px)";
+        chatBobbles.style.top = "25%";
+        profile.style.top = "55%";
     }, 4500);
 });
 
